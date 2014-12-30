b2d2ac38b83c9a88f102fba092bcac72a7ed3262
Ronald Johnson <ronald@home>
2014-12-30 15:42:09 -0600

    Listing 3.7

--- src/main/webapp/js/spa.js
+++ src/main/webapp/js/spa.js
@@ -2,7 +2,7 @@
 // Root namespace module
 
 /*jslint browser: true, continue: true,
-devel: true, indent: 2, maxerr: 50,
+devel: true, indent: 4, maxerr: 50,
 newcap: true, nomen: true, plusplus: true,
 regexp: true, sloppy: true, vars: false,
 white: true
