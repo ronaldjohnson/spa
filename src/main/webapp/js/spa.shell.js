d83da251b416f93aaa6591eb42d8fb8cd6d1e3b1
Ronald Johnson <ronald@home>
2015-01-01 23:36:16 -0600

    Listing 3.11

--- src/main/webapp/js/spa.shell.js
+++ src/main/webapp/js/spa.shell.js
@@ -9,28 +9,76 @@
     regexp : true, sloppy: true, vars : false,
     white : true
 */
-/*global $, spa */
-spa.shell = (function () {
-    var configMap = {},
+/*global $, spa, window */
+spa.shell = (function (global) {
+    var configMap = {
+            chat_extend_time: 1000,
+            chat_retract_time: 300,
+            chat_extend_height: 450,
+            chat_retract_height: 15
+        },
         stateMap = { '$container' : null },
         jqueryMap = {},
-        setJqueryMap, initModule;
+
+        setJqueryMap, toggleChat, initModule;
 
     setJqueryMap = function () {
         var $container = stateMap.$container;
-        jqueryMap = { '$container' : $container };
+        jqueryMap = {
+            '$container' : $container,
+            '$chat': $container.find( '.spa-shell-chat' )
+        };
     };
 
+    toggleChat = function ( do_extend, callback ) {
+        var
+            px_chat_ht = jqueryMap.$chat.height(),
+            is_open = px_chat_ht === configMap.chat_extend_height,
+            is_closed = px_chat_ht === configMap.chat_retract_height,
+            is_sliding = ! is_open && ! is_closed;
+
+        // avoid race condition
+        if( is_sliding ) { return false; }
+
+        // Begin extend chat slider
+        if ( do_extend ) {
+            jqueryMap.$chat.animate(
+                { height: configMap.chat_extend_height },
+                configMap.chat_extend_time,
+                function () {
+                    if( callback ) { callback( jqueryMap.$chat ); }
+                }
+            );
+            return true;
+        }
+
+        jqueryMap.$chat.animate(
+            { height: configMap.chat_retract_height },
+            configMap.chat_retract_time,
+            function () {
+                if ( callback ) { callback( jqueryMap.$chat ); }
+            }
+        );
+
+        return true;
+    };
+
+
     initModule = function ( $container ) {
+        // load HTML and map jQuery collections
         $container.load('layout.html', function (html) {
             configMap.main_html = html;
         });
         stateMap.$container = $container;
         $container.html( configMap.main_html );
         setJqueryMap();
+
+        // test toggle
+        global.setTimeout(function () {toggleChat( true ); }, 3000);
+        global.setTimeout(function () {toggleChat( false ); }, 8000);
     };
 
     return {
         initModule: initModule
     };
-}());
+}(window));
